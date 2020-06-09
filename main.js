22af3cb38595874bac4e20a014f72b6390041c76
Andy Mikulski <andy.mikulski@gmail.com>
2020-06-09 09:19:18 -0600

    remove password

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-if (!sessionStorage.getItem('hcnp')) {
-  document.body.style.display = 'none';
-  if (prompt("password") !== 'hardcore') {
-    window.location.href = 'https://liquiddeath.com/'
-  } else {
-    sessionStorage.setItem('hcnp', '1');
-    document.body.style.display = 'block';
-  }
-}
+// if (!sessionStorage.getItem('hcnp')) {
+//   document.body.style.display = 'none';
+//   if (prompt("password") !== 'hardcore') {
+//     window.location.href = 'https://liquiddeath.com/'
+//   } else {
+//     sessionStorage.setItem('hcnp', '1');
+//     document.body.style.display = 'block';
+//   }
+// }
 
 // https://ld-hcnp.s3.us-east-2.amazonaws.com/01/01_thumbs-3.jpg
 
